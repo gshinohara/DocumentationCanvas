3ad78e58877228798c4f96c597809a9a371cf78d
Gaku Shinohara <gaku.shinohara@vicc.jp>
2024-08-28 18:41:08 +0900

    Add a loading instruction

---
+++ DocumentationCanvas/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace DocumentationCanvas
+{
+    public class AssemblyInfo : GH_AssemblyInfo
+    {
+        public override string Name => EnvParam.PluginName;
+        public override Bitmap Icon => null;
+        public override string Description => "";
+
+        public override Guid Id => new Guid("2c0a0db0-07d0-4675-84b7-e6a37ef076c3");
+        public override string AuthorName => "Gaku Shinohara";
+        public override string AuthorContact => "";
+    }
+
+    internal static class EnvParam
+    {
+        public static string PluginName => "DocumentationCanvas";
+    }
+}
---
+++ DocumentationCanvas/AssemblyInitialization.cs
@@ -0,0 +1,62 @@
+﻿using Eto.Forms;
+using Grasshopper.Kernel;
+using System;
+
+namespace DocumentationCanvas
+{
+    public class AssemblyInitialization : GH_AssemblyPriority
+    {
+        private class LoadSelector : Dialog<GH_LoadingInstruction>
+        {
+            public LoadSelector()
+            {
+                Title = "Load?";
+
+                Padding = 5;
+                Width = 200;
+                AutoSize = true;
+                Topmost = true;
+
+                Location = (Eto.Drawing.Point)Screen.Bounds.Center;
+
+                DynamicLayout layout = new DynamicLayout
+                {
+                    Padding = 3,
+                    Spacing = new Eto.Drawing.Size(5, 10),
+                };
+
+                EventHandler<EventArgs> buttonEvent = (sender, e) => Close((GH_LoadingInstruction)(sender as Button).Tag);
+
+                Button button_Yes = new Button(buttonEvent)
+                {
+                    Text = "Yes",
+                    Tag = GH_LoadingInstruction.Proceed,
+                };
+
+                Button button_No = new Button(buttonEvent)
+                {
+                    Text = "No",
+                    Tag = GH_LoadingInstruction.Abort,
+                };
+
+                layout.AddSeparateRow(new Label { Text = $"Do you want to load {EnvParam.PluginName} plug-in?", Wrap = WrapMode.Word });
+                layout.AddSeparateRow(button_Yes, null, button_No);
+
+                Content = layout;
+            }
+
+            protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+            {
+                if (e.Cancel)
+                    Result = GH_LoadingInstruction.Abort;
+                base.OnClosing(e);
+            }
+        }
+
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            LoadSelector form = new LoadSelector();
+            return form.ShowModal(Rhino.UI.RhinoEtoApp.MainWindow);
+        }
+    }
+}
